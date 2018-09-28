6334c71474257d40f42e3ecf7f040cc81addee9e
Ryan Scott <rscott@roblox.com>
2018-09-28 16:58:14 -0700

    Added mongo logging support (db.collection = roblox.logging)

--- logger.js
+++ logger.js
@@ -8,6 +8,72 @@ const path = require('path');
 const url = require('url');
 const querystring = require('querystring');
 const fs = Promise.promisifyAll(require('fs'));
+const MongoClient = require('mongodb').MongoClient;
+
+function logToConsole(msg) {
+  console.log("%s %s, %s, %s, %s, %s, %s, %s",
+              msg.time, msg.game, msg.place, msg.job, msg.user,
+              msg.level, msg.channel, msg.message);
+}
+
+function logToDB(data, doConsole) {
+  var mongo = MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    const db = client.db('roblox');
+
+    var messages = data.messages;
+    var logMessages = [];
+
+    for(var i = 0; i < messages.length; ++i) {
+      var msg = messages[i];
+
+      if(msg.player) {
+        // Client
+        var clientMessages = msg.messages;
+        for(var j = 0; j < clientMessages.length; ++j) {
+          var clientMsg = clientMessages[j];
+
+          var out = {time: clientMsg[0],
+                     game: data.game,
+                     place: data.place,
+                     job: data.job,
+                     user: msg.player,
+                     level: clientMsg[1],
+                     channel: clientMsg[2],
+                     message: clientMsg[3]}
+
+          //
+          logMessages.push(out);
+          
+          if(doConsole) {
+            logToConsole(out);
+          }
+        }
+      }
+      else {
+        // Server
+        var out = {time: msg[0],
+                   game: data.game,
+                   place: data.place,
+                   job: data.job,
+                   level: msg[1],
+                   channel: msg[2],
+                   message: msg[3]}
+
+        logMessages.push(out);
+        
+        if(doConsole) {
+          logToConsole(out)
+        }
+      }
+    }
+
+    // TODO: Promisify and async
+    const collection = db.collection('logging');
+    collection.insertMany(logMessages);
+    
+    client.close();
+  });
+}
 
 var server = http.createServer(function(request, response) {
   var mapUrl = url.parse(request.url);
@@ -26,30 +92,7 @@ var server = http.createServer(function(request, response) {
     response.end();
 
     var data = JSON.parse(rawdata);
-    var messages = data.messages;
-
-    for(var i = 0; i < messages.length; ++i) {
-      var msg = messages[i];
-
-      if(msg.player) {
-        // Client
-        var clientMessages = msg.messages;
-        for(var j = 0; j < clientMessages.length; ++j) {
-          var clientMsg = clientMessages[j];
-          console.log("CLIENT: %s %s, %s, %s, %s, %s, %s, %s",
-                      clientMsg[0],
-                      data.game, data.place, data.job, msg.player,
-                      clientMsg[1], clientMsg[2], clientMsg[3]);
-        }
-      }
-      else {
-        // Server
-        console.log("SERVER: %s %s, %s, %s, %s, %s, %s, %s",
-                    msg[0],
-                    data.game, data.place, data.job, undefined,
-                    msg[1], msg[2], msg[3]);
-      }
-    }
+    logToDB(data, true);
   });
   
 }).listen(9900);
